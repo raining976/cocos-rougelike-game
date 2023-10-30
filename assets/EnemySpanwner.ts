590e946aa74c02e296b0de0114b1a622cb295c90
chenlan7064 <2152409138@qq.com>
2023-10-29 22:38:00 -0400

    feat:增加缓动特性，实现敌人对目标的平滑追踪，微调状态机参数
    增加缓动特性，实现敌人对目标的平滑追踪，微调状态机参数

--- assets/EnemySpanwner.ts
+++ assets/EnemySpanwner.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Prefab, randomRange,Vec3, instantiate, macro,game, TiledUserNodeData} from 'cc';
+import { _decorator, Component, Node, Prefab, randomRange,Vec3, instantiate, macro,game, TiledUserNodeData, tween} from 'cc';
 import { EnemySettings } from './EnemySettings';
 import { Enemy } from './Enemy';
 const { ccclass, property } = _decorator;
@@ -11,40 +11,28 @@ export class EnemySpanwner extends Component {
     // private TargetNode:Node;
     private IdToSettings=new Map<string,EnemySettings>();
     private SpanwnerDelay:number=1.0;//怪物生成延迟
-    private interval:number=1.0;//状态机AI执行间隔
-    private AIdelay:number=1.0;//状态机AI执行延迟
+    private interval:number=5.0;//状态机AI思考间隔
+    private AIdelay:number=1.0;//状态机AI思考延迟
     private attackdelay:number=0.1;//伤害判定延迟
     private localpos:Vec3=new Vec3();
     start() {
-        console.log("stlocalpos: ",this.localpos);
-        this.localpos.x=360;
-        this.localpos.y=308.4466019417476;//先将就着...后面会改的
-        console.log("stlocalpos: ",this.localpos);
+        this.localpos.x=this.node.worldPosition.x;
+        this.localpos.y=this.node.worldPosition.y;
     }
 
     update(deltaTime: number) {
         this.curDelay+=deltaTime;
-        // console.log("222",randomRange(300, 600)* (Math.random() < 0.5 ? 1 : -1));
-        console.log(this.curDelay);
         if(this.curDelay>this.SpanwnerDelay){
             this.curDelay=0;
             const posX: number = randomRange(90, 180) * (Math.random() < 0.5 ? 1 : -1);
             const posY: number = randomRange(300, 600) * (Math.random() < 0.5 ? 1 : -1);
-            // console.log(this.curDelay,posX,posY);
-             console.log(this.TargetNode.worldPosition);
             const spawnPosition = new Vec3();
             spawnPosition.x = this.TargetNode.worldPosition.x + posX-this.node.worldPosition.x;
             spawnPosition.y = this.TargetNode.worldPosition.y + posY-this.node.worldPosition.y;
-            // console.log(this.enemies);
-            // console.log("spawnPosition is: ",spawnPosition);
             const enemynode:Node=instantiate(this.enemies);
             enemynode.setWorldPosition(spawnPosition);
             const one_enmey=enemynode.addComponent(Enemy)//敌人配置读取
-            console.log("enemynodepos: ",enemynode.worldPosition);
             this.node.addChild(enemynode);
-            // console.log("thisnodePosition is: ",this.node.worldPosition);
-            // console.log("enemynodePosition is: ",enemynode.worldPosition);
-            // console.log(this.node.children[2]);
             this.schedule(this.StateAI.bind(enemynode,this.TargetNode,this.localpos),this.interval,macro.REPEAT_FOREVER,this.AIdelay);
         }
         
@@ -52,8 +40,6 @@ export class EnemySpanwner extends Component {
 
     StateAI(targetnode:Node,localpos:Vec3){
         // console.log(this.worldPosition);
-        console.log("tnpos is: ",targetnode.worldPosition);
-        console.log("thispos is: ",this.worldPosition);
         const enemytype=this.getComponent(Enemy);
         if(targetnode ==null){//目标节点不存在
             return;
@@ -66,19 +52,7 @@ export class EnemySpanwner extends Component {
         // const canAttack=//攻击间隔判定
         const distance=Vec3.distance(this.worldPosition,targetnode.worldPosition);//攻击间隔判定//不用管报错，VScode这里识别不出bind绑定
         if(distance>enemytype.getattackrange()){
-            let temp=new Vec3();
-            Vec3.subtract(temp, targetnode.worldPosition, this.worldPosition);
-            temp.normalize();
-            console.log("temp1= ",temp);
-            temp.x=temp.x*10;
-            temp.y=temp.y*10;
-            temp.z=temp.z*10;
-            console.log("temp2= ",temp);
-            console.log("pos1:",this.worldPosition);
-            Vec3.add(temp,temp,this.worldPosition);
-            Vec3.subtract(temp,temp,localpos);
-            this.setPosition(temp);
-            console.log("pos2:",this.worldPosition);
+            tween(this).to(5,{worldPosition:targetnode.worldPosition},{easing:"smooth"}).start();   
         }
     }
     
