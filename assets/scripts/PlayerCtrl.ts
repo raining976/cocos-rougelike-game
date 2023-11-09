bd81eebb1c07414de096fb7ff609fbb6542e7912
raining <running.zhang@foxmail.com>
2023-11-09 22:33:53 +0800

    fix:添加移动动画测试

--- assets/scripts/PlayerCtrl.ts
+++ assets/scripts/PlayerCtrl.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Vec3, CCInteger } from 'cc';
+import { _decorator, Component, Node, Vec3, CCInteger,Animation } from 'cc';
 import { JoyStick } from './JoyStick';
 const { ccclass, property } = _decorator;
 
@@ -15,21 +15,57 @@ export class PlayerCtrl extends Component {
     @property({ type: CCInteger })
     status: number = 0;
 
+    moveDir:string = null // l左 r右
+
     joyStick: JoyStick | null = null;//摇杆控制组件
 
+    runAnim = null
+
+
     start() {
+        this.runAnim = this.node.getComponent(Animation)
+        console.log('this.runAnim',this.runAnim)
         this.joyStick = this.joyStickPanel.getComponent(JoyStick);
     }
 
     update(deltaTime: number) {
-        this.movePlayer(deltaTime);
+        if(this.isNeedMove(this.joyStick.dir)){
+            this.getMoveDir()
+            let dirBackup = this.joyStick.dir.clone();
+            let dis = dirBackup.multiplyScalar(this.maxSpeed * this.joyStick.ratio);
+            this.movePlayer(dis)
+            this.playAnim()
+        }
     }
     //角色移动
-    movePlayer(deltaTime: number) {
-        let dirBackup = this.joyStick.dir.clone();
-        let dis = dirBackup.multiplyScalar(this.maxSpeed * this.joyStick.ratio);
-        this.node.setPosition(this.node.position.add(dis));
+    movePlayer(dis:Vec3) {
+            this.node.setPosition(this.node.position.add(dis));
     }
+
+    // 判断是否要移动
+    isNeedMove(v3: Vec3) {
+        return v3.length() != 0
+    }
+
+    // 返回运动方向
+    getMoveDir(){
+        let v3 = this.joyStick.dir
+        if(v3.x > 0) this.moveDir = 'r'
+        else if(v3.x < 0) this.moveDir = 'l'
+        console.log('this.moveDir',this.moveDir)
+    }
+    
+
+    // 播放动画
+    playAnim(){
+        if(this.moveDir == 'r'){
+            this.runAnim.play('runRightAnim')
+        }else if(this.moveDir == 'l'){
+
+        }
+    }
+
+
 }
 
 
