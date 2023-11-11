bbc83040c88824ae6b171eee7d22fb7a7f43c4ca
raining <running.zhang@foxmail.com>
2023-11-11 21:47:59 +0800

    feat:添加主角左右移动的动画

--- assets/scripts/PlayerCtrl.ts
+++ assets/scripts/PlayerCtrl.ts
@@ -13,7 +13,7 @@ export class PlayerCtrl extends Component {
 
     //状态属性，0为静止，1为移动
     @property({ type: CCInteger })
-    status: number = 0;
+    moveStatus: number = 0;
 
     moveDir:string = null // l左 r右
 
@@ -24,22 +24,22 @@ export class PlayerCtrl extends Component {
 
     start() {
         this.runAnim = this.node.getComponent(Animation)
-        console.log('this.runAnim',this.runAnim)
         this.joyStick = this.joyStickPanel.getComponent(JoyStick);
     }
 
     update(deltaTime: number) {
         if(this.isNeedMove(this.joyStick.dir)){
-            this.getMoveDir()
+            this.moveStatus = 1
             let dirBackup = this.joyStick.dir.clone();
             let dis = dirBackup.multiplyScalar(this.maxSpeed * this.joyStick.ratio);
             this.movePlayer(dis)
+            // this.setPanelPos()
             this.playAnim()
-        }
+        }else this.moveStatus = 0
     }
     //角色移动
     movePlayer(dis:Vec3) {
-            this.node.setPosition(this.node.position.add(dis));
+        this.node.setPosition(this.node.position.add(dis));
     }
 
     // 判断是否要移动
@@ -49,20 +49,33 @@ export class PlayerCtrl extends Component {
 
     // 返回运动方向
     getMoveDir(){
+        let moveDir
         let v3 = this.joyStick.dir
-        if(v3.x > 0) this.moveDir = 'r'
-        else if(v3.x < 0) this.moveDir = 'l'
-        console.log('this.moveDir',this.moveDir)
+        if(v3.x > 0) moveDir = 'r'
+        else if(v3.x < 0) moveDir = 'l'
+        else moveDir = this.moveDir
+        return moveDir
     }
     
 
     // 播放动画
     playAnim(){
-        if(this.moveDir == 'r'){
-            this.runAnim.play('runRightAnim')
-        }else if(this.moveDir == 'l'){
-
+        let curMoveDir  = this.getMoveDir()
+        if(this.moveDir != curMoveDir){
+            if(curMoveDir == 'r' ){
+                this.runAnim.play('runRightAnim')
+            }else if(curMoveDir == 'l'){
+                this.runAnim.play('runLeftAnim')
+            }
+            this.moveDir = this.getMoveDir()
         }
+        
+    }
+
+    setPanelPos(){
+        let deltaPos = this.joyStickPanel.worldPosition.subtract(this.node.worldPosition)
+        this.joyStickPanel.setPosition(deltaPos)
+       
     }
 
 
