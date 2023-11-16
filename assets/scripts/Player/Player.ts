4b24bd8d1f21d1227dfa472958c5f427d99de16f
Ttdotd <rundong.zhang350@gmail.com>
2023-11-16 23:16:43 +0800

    feat: 碰撞检测

--- assets/scripts/Player/Player.ts
+++ assets/scripts/Player/Player.ts
@@ -10,7 +10,11 @@ export class Player extends Component {
 
     private settings = PlayerAttr;//角色属性组配置
     private id: string = "1" // 角色id
-    private health: number = 100;//血条上限
+    private level: number = 0;
+    private curHealth: number = 100;//当前血量
+    private maxHealth: number = 100;//血量上限
+    private curExp: number = 100;//当前经验值
+    private maxExp: number = 100;//经验值上限
     private damage: number = 10; // 攻击伤害
     private speed: number = 100; // 移动速度
     private attackrange: number = 100; // 攻击范围
@@ -18,21 +22,57 @@ export class Player extends Component {
     start() {
         const playerName: string = "Yellow";
         this.init(playerName);
-
     }
 
     update(deltaTime: number) {
         //更新血量
     }
+
     public init(playerName: string): void {
         this.id = this.settings[playerName].id;
-        this.health = this.settings[playerName].health;
+        this.level = 0;
+        this.maxHealth = this.settings[playerName].health;
+        this.curHealth = this.maxHealth;
+        this.maxExp = 100;
+        this.curExp = this.maxExp;
         this.damage = this.settings[playerName].damage;
         this.speed = this.settings[playerName].speed;
         this.attackrange = this.settings[playerName].attackrange;
     }
+
+
+    /**
+     * 经验值满后，提升等级
+     * @param overflowExp 溢出经验值
+     */
+    public improveLevel(overflowExp: number) {
+        //经验、等级
+        this.level ++;
+        this.maxExp *= 2;
+        this.curExp = overflowExp;
+        //属性提升
+        //TODO:
+    }
+
     public getSpeed() {
         return this.speed;
+    }
+
+    public setCurHealth(newHealth: number) {
+        this.curHealth = newHealth;
+    }
+    public getCurHealth() {
+        return this.curHealth;
+    }
+
+    public setCurExp(newExp: number) {
+        this.curExp = newExp;
+    }
+    public getCurExp() {
+        return this.curExp;
+    }
+    public getMaxExp() {
+        return this.maxExp;
     }
 }
 
--- assets/scripts/Player/PlayerCtrl.ts
+++ assets/scripts/Player/PlayerCtrl.ts
@@ -1,6 +1,7 @@
-import { _decorator, Component, Node, Vec3, CCInteger, Animation, input, Input, EventTouch, KeyCode, EventKeyboard } from 'cc';
+import { _decorator, Component, Node, Vec3, CCInteger, Animation, input, Input, EventTouch, KeyCode, EventKeyboard, Collider2D, Contact2DType, Collider, IPhysics2DContact } from 'cc';
 import { JoyStick } from './JoyStick';
 import { Player } from './Player';
+import { Enemy } from '../Enemy/Enemy'
 const { ccclass, property } = _decorator;
 
 @ccclass('PlayerCtrl')
@@ -10,7 +11,6 @@ export class PlayerCtrl extends Component {
     joyStickPanel: Node | null = null;
 
     moveStatus: number = 0; // 移动状态 0 静止 1移动
-
     moveDir: string = null // l左 r右
 
     curDir: Vec3 = new Vec3() // 当前移动方向向量 
@@ -18,26 +18,120 @@ export class PlayerCtrl extends Component {
     joyStick: JoyStick | null = null;//摇杆控制组件
     playerAttr: Player | null = null;//角色属性组件
 
-    runAnim:Animation = null // 人物动画
+    runAnim: Animation = null // 人物动画
 
+    damageDelay: number = 1;
     start() {
-        this.runAnim = this.node.getComponent(Animation)
+        this.runAnim = this.node.getComponent(Animation);
         this.joyStick = this.joyStickPanel.getComponent(JoyStick);
         this.playerAttr = this.node.getComponent(Player);
+
+        //自定义定时器
+        this.schedule(this.reduceHealth, this.damageDelay, )
     }
 
     onLoad() {
-        input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
-        input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
+        this.initInputEvent();
+        this.initCollider();
     }
-    onDestroy() {
-        input.off(Input.EventType.KEY_DOWN, this.onKeyDown, this);
-        input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
 
+    onDestroy() {
+        this.destoryInputEvent();
+        this.destroyCollider();
     }
 
     /**
-     * 键盘按下监听 
+     * 注册碰撞事件
+     */
+    initCollider() {
+        let collider = this.getComponent(Collider2D);
+        if (collider) {
+            collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+            collider.on(Contact2DType.PRE_SOLVE, this.onPreSolve, this);
+        }
+    }
+
+    /**
+     * 销毁碰撞事件
+     */
+    destroyCollider() {
+        let collider = this.getComponent(Collider2D);
+        if (collider) {
+            collider.off(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+            collider.off(Contact2DType.PRE_SOLVE, this.onPreSolve, this);
+        }
+    }
+
+    /**
+     * 注册输入事件
+     */
+    initInputEvent() {
+        input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
+        input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
+    }
+
+    /**
+     * 销毁输入事件
+     */
+    destoryInputEvent() {
+        input.off(Input.EventType.KEY_DOWN, this.onKeyDown, this);
+        input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
+    }
+
+    /**
+     * 初次碰撞回调
+     */
+    onBeginContact(selfCollier: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+        // //tag=1 代表与小怪碰撞 tag=2 代表与经验球碰撞
+        // if (otherCollider.tag == 1) {
+        //     this.reduceHealth(otherCollider.node.getComponent(Enemy).getdamage());
+        // } else if (otherCollider.tag == 2) {
+        //     //TODO:等经验球做完后，调整这里传递的参数
+        //     this.increaseExp(1);
+        // }
+    }
+
+    /**
+     * 碰撞持续回调
+     * @param selfCollier
+     * @param otherCollider 
+     * @param contact 
+     */
+    onPreSolve(selfCollier: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null){
+        //tag=1 代表与小怪碰撞 tag=2 代表与经验球碰撞
+        if (otherCollider.tag == 1) {
+            this.reduceHealth(otherCollider.node.getComponent(Enemy).getdamage());
+        } else if (otherCollider.tag == 2) {
+            //TODO:等经验球做完后，调整这里传递的参数
+            this.increaseExp(1);
+        }
+    }
+
+    /**
+     * 在与小怪碰撞后，降低角色血量
+     * @param delt 血量降低值
+     */
+    reduceHealth(delt: number) {
+        let newHealth = this.playerAttr.getCurHealth() - delt;
+        if (newHealth < 0) {
+            console.log("Game over");
+        }
+        this.playerAttr.setCurHealth(newHealth);
+    }
+
+    /**
+     * 与经验值碰撞后，增加经验值
+     * @param delt 经验增加值
+     */
+    increaseExp(delt: number) {
+        let newExp = this.playerAttr.getCurExp();
+        if (newExp > this.playerAttr.getMaxExp()) {
+            this.playerAttr.improveLevel(newExp - this.playerAttr.getMaxExp());
+        }
+        this.playerAttr.setCurExp(newExp);
+    }
+    /**
+     * 键盘按下监听
      * @param e 
      */
     onKeyDown(e: EventKeyboard) {
@@ -149,10 +243,6 @@ export class PlayerCtrl extends Component {
         }
 
     }
-
-
-
-
 }
 
 
