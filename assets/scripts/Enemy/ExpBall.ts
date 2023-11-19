4ba848f3a50a83494e1e9a9c55a793b680c1d952
吴丁煜 <wudingyu@wudingyudeMacBook-Pro.local>
2023-11-19 15:23:10 +0800

    测试

--- assets/scripts/Enemy/ExpBall.ts
+++ assets/scripts/Enemy/ExpBall.ts
@@ -4,22 +4,25 @@ const { ccclass, property } = _decorator;
 @ccclass('ExpBall')
 export class ExpBall extends Component {
     @property(Prefab)
-    expBallPrefab: Prefab = null;  // 经验球的预制体
+    expBallPrefab: Prefab;  // 经验球的预制体
+    @property(Node) private TargetNode:Node;//玩家节点为TargetNode
 
     generateExpBall() {
-        // 获取画布范围，画布范围好像是(640, 1000)吧？
-        const canvasSize = new Vec3(640, 1000, 0);
+         //获取画布范围，画布范围好像是(640, 1000)吧？
+        //const canvasSize = new Vec3(640, 1000, 0);
 
         // 在画布范围内随机生成经验球的位置
-        const randomX = Math.random() * canvasSize.x - canvasSize.x / 2;
-        const randomY = Math.random() * canvasSize.y - canvasSize.y / 2;
-        const position = new Vec3(randomX, randomY, 0);
+        //const randomX = Math.random() * canvasSize.x - canvasSize.x / 2;
+        //const randomY = Math.random() * canvasSize.y - canvasSize.y / 2;
+        //const position = new Vec3(randomX, randomY, 0);
 
         // 实例化经验球预制体，并设置位置等信息
-        const expBallNode = instantiate(this.expBallPrefab);
-        expBallNode.setPosition(position);
-        // 将经验球添加到场景中
-        this.node.addChild(expBallNode);
+        let expBallNode = instantiate(this.expBallPrefab);
+        // 将经验球添加到画布
+        expBallNode.parent = this.node;
+        //获得位置
+        let new_position = new Vec3(this.node.position);
+        expBallNode.setWorldPosition(new_position);
 
         // 在3秒后销毁经验球，回调函数，延迟执行，消除节点
         this.scheduleOnce(() => {
@@ -29,7 +32,7 @@ export class ExpBall extends Component {
 
     start() {
         // 调用生成经验球的方法示例
-        this.generateExpBall();
+        //this.generateExpBall();
     }
 
     update(deltaTime: number) {
