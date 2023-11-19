07a4e17dfdd7e903b501398217720d0b31c31fb8
吴丁煜 <wudingyu@wudingyudeMacBook-Pro.local>
2023-11-19 13:58:41 +0800

    feat: 经验球脚本测试

---
+++ assets/scripts/Enemy/ExpBall.ts
@@ -0,0 +1,48 @@
+import { _decorator, Component, Node, Prefab, instantiate, Vec3, find } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('ExpBall')
+export class ExpBall extends Component {
+    @property(Prefab)
+    expBallPrefab: Prefab = null;  // 经验球的预制体
+
+    generateExpBall() {
+        // 获取画布范围，画布范围好像是(640, 1000)吧？
+        const canvasSize = new Vec3(640, 1000, 0);
+
+        // 在画布范围内随机生成经验球的位置
+        const randomX = Math.random() * canvasSize.x - canvasSize.x / 2;
+        const randomY = Math.random() * canvasSize.y - canvasSize.y / 2;
+        const position = new Vec3(randomX, randomY, 0);
+
+        // 实例化经验球预制体，并设置位置等信息
+        const expBallNode = instantiate(this.expBallPrefab);
+        expBallNode.setPosition(position);
+        // 将经验球添加到场景中
+        this.node.addChild(expBallNode);
+
+        // 在3秒后销毁经验球，回调函数，延迟执行，消除节点
+        this.scheduleOnce(() => {
+            expBallNode.destroy();
+        }, 3);
+    }
+
+    start() {
+        // 调用生成经验球的方法示例
+        this.generateExpBall();
+    }
+
+    update(deltaTime: number) {
+        // 怪物死亡时生成经验球
+        //if (this.isDead()) {
+            //this.generateExpBall();
+        //}
+        // 测试：一直生成经验球
+        this.generateExpBall();
+    }
+    //外部判断
+    isDead() {
+        // 判断怪物是否死亡，并返回一个布尔值
+        return true; // 示例代码，假设怪物始终为死亡状态
+    }
+}
