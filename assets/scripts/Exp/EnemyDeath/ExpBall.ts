cc8e962cc8e84a58cfd9aef942ff7428280bf884
raining <running.zhang@foxmail.com>
2023-11-26 16:10:34 +0800

    feat:完善经验球实体类的脚本

--- assets/scripts/Exp/EnemyDeath/ExpBall.ts
+++ assets/scripts/Exp/EnemyDeath/ExpBall.ts
@@ -4,28 +4,36 @@ const { ccclass, property } = _decorator;
 
 @ccclass('ExpBall')
 export class ExpBall extends Component {
-    
-    protected settings=ExpBallAttr;//敌人属性组配置
-    protected size:number;
-    protected color:number;
-    protected value:number;
-    
-    private elapsedTime: number = 0;    //计时器
-    private timerStarted: boolean = false;//标志计时器是否启动
 
+    protected settings = ExpBallAttr;//敌人属性组配置
+    protected size: number; // 经验球大小
+    protected color: number; // 经验球颜色
+    protected value: number; // 经验球值
 
-    
     start() {
-        //1000s后自动删除经验球
-        setTimeout(() => {
-            this.node.destroy();
-        }, 10000);
-
+        let expBallName = 'Small'
+        this.init(expBallName)
+        this.autoDestroy()
     }
-    update(deltaTime: number) {
+
+    init(expBallName: string) {
+        this.size = this.settings[expBallName].size;
+        this.color = this.settings[expBallName].color;
+        this.value = this.settings[expBallName].value;
+    }
+
+    getValue() {
+        return this.value;
+    }
+
+    autoDestroy() {
+        //10s后自动删除经验球
+        setTimeout(() => {
+            this.node && this.node.destroy();
+        }, 10000);
     }
 }
 
 
-    
+
 
