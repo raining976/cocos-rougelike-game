5b1cd5b40e442396c010f3604c41ea905a51e0a1
chenlan7064 <2152409138@qq.com>
2023-12-04 09:53:10 -0500

    refactor:完全实现状态模式，应用FSM对敌人状态进行管理
    通过应用状态模式，将敌人行为封装至状态对象中，有利于后续程序的扩展，能很容易地配置和修改不同敌人的个性化行为，极大地提高了代码的灵活性、可重用性、可读性、扩展性与可维护性

---
+++ assets/scripts/utils/FSM/ProxyClass.ts
@@ -0,0 +1,22 @@
+import { _decorator, Component, Node } from 'cc';
+import { Storage } from './Storage';
+const { ccclass, property } = _decorator;
+
+@ccclass('ProxyClass')
+export class ProxyClass {
+    /**
+     * 代理构建方法
+     * @param className 动态类名称
+     * @param option 动态类创建参数
+     */
+   constructor(className: string, option1:any, option2:any) {
+       //一个简单的异常判断，如果存储类中不存在此类 则抛出异常提醒
+    //   console.log(className,option);
+      if (Storage[className] === undefined || Storage[className] === null) {
+        throw new Error(`未找到 className：${className} 对应实现`);
+      }
+      //从存放对象上找出对应class 创建即可
+      return new Storage  [className](option1,option2) 
+    }
+   }
+
