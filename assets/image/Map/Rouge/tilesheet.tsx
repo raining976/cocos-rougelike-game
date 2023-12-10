4346562b465684ad75b144f0d3080ad4dbf13bff
hyblif <hyblif@163.com>
2023-12-10 21:11:55 +0800

    feat: Tiled Map added
    change former map asset to tmx

---
+++ assets/image/Map/Rouge/tilesheet.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tilesheet" tilewidth="16" tileheight="16" tilecount="1960" columns="20">
+ <image source="assets/tilesheet.png" width="320" height="1568"/>
+ <tile id="220">
+  <animation>
+   <frame tileid="240" duration="100"/>
+   <frame tileid="241" duration="100"/>
+   <frame tileid="242" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
